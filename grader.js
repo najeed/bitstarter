7e8ab88caabab3fa4f653b39f28e7035ebdad068
Ubuntu <ubuntu@ip-172-31-47-246.us-west-2.compute.internal>
2013-07-08 10:01:10 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -31,7 +31,7 @@ var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://aqueous-earth-9813.herokuapp.com";
+var URL_DEFAULT = "http://intense-earth-7167.herokuapp.com/";
 var DUMMY_HTML = "dummy.html";
 var willPrint = false;
 
